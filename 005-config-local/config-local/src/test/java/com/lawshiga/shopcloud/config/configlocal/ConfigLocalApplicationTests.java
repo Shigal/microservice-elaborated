b40403d91fcd474cc79109db8cdbc3d715228aeb
Shigal <shiglaws@gmail.com>
2022-12-12 12:53:21 +0530

    first commit

---
+++ 005-config-local/config-local/src/test/java/com/lawshiga/shopcloud/config/configlocal/ConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawshiga.shopcloud.config.configlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
